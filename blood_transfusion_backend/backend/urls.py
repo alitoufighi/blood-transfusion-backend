b5f3df6fb47841ba00d7a15000ceed7d980ccf52
Mohammad Ali Toufighi <alitoumob@gmail.com>
2020-01-28 08:12:25 +0330

    beautified

--- blood_transfusion_backend/backend/urls.py
+++ blood_transfusion_backend/backend/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('transporters', views.list_blood_transporters, name='list_blood_transporters'),
+    path('donor', views.donor_info, name='view_donor_info'),
+    path('necessaryBloodProductsInCity', views.necessary_blood_products_in_city, name='view_necessary_blood_products_in_city'),
+    path('newDonor', views.new_donor, name='view_new_donor'),
+
+    path('', views.home_page, name='view_homeـpage'),
 ]
--- blood_transfusion_backend/backend/views.py
+++ blood_transfusion_backend/backend/views.py
@@ -12,7 +12,10 @@ def execute(query):
     conn = pyodbc.connect(f'Driver={{SQL Server}};Server={server};Database={db};Trusted_Connection=yes;')
     crsr = conn.cursor()
     crsr.execute(query)
-    rows = crsr.fetchall()
+    if not 'INSERT' in query:
+        rows = crsr.fetchall()
+    else:
+        rows = None
     description = crsr.description
     crsr.close()
     conn.close()
@@ -20,17 +23,49 @@ def execute(query):
 
 
 def home_page(request):
-    return render('backend/home.html', {})
+    return render(request, 'backend/home.html', {})
 
-def list_blood_transporters(request):
-    print("Yay")
-    # cursor = connection.cursor()
-    # cursor.execute("EXEC ListBloodTransporters")
-    rows, desc = execute("EXEC ListBloodTransporters")
+def donor_info(request):
+    national_id = request.GET.get('national_id', None)
+    if national_id is None:
+        return render(request, 'backend/home.html', {'error': 'لطفاً کد ملی را وارد کنید.'})
+    rows, desc = execute(f"SELECT * FROM BloodTransporter WHERE nationalId={national_id}")
+    desc = [d[0] for d in desc]
+    donor = rows[0]
     print('------------')
     print(rows, desc)
     print('------------')
-    # return render('backend/list_blood_transporters.html', rows)
+    return render(request, 'backend/donor_info.html', {'result': donor, 'titles': desc})
+
+def new_donor(request):
+    if request.method == 'POST':
+        national_id = request.POST.get('nationalId', None)
+        first_name = request.POST.get('firstName', None)
+        last_name = request.POST.get('lastName', None)
+        blood_type = request.POST.get('bloodType', None)
+        print(f"'{national_id}' '{first_name}' '{last_name}' '{blood_type}'")
+        if '' in [national_id, first_name, last_name, blood_type]:
+            return render(request, 'backend/new_donor.html', {'message': 'لطفاً تمامی فیلدهای مربوطه را پر کنید.'})
+        try:
+            rows, desc = execute(f"INSERT INTO BloodTransporter (nationalId, firstName, lastName, bloodType) VALUES (N'{national_id}', N'{first_name}', N'{last_name}', N'{blood_type}')")
+        except pyodbc.IntegrityError:
+            return render(request, 'backend/new_donor.html', {'message': 'نع!.'})
+        print(rows, desc)
+        return render(request, 'backend/home.html', {'message': 'اهداگر جدید با موفقیت اضافه شد.'})
+    else:
+        return render(request, 'backend/new_donor.html', {})
+
+
+def necessary_blood_products_in_city(request):
+    city_name = request.GET.get('city_name', None)
+    blood_type = request.GET.get('blood_type', None)
+    if None in [city_name, blood_type]:
+        return render(request, 'backend/home.html', {'error': 'لطفاً مقادیر خواسته شده را وارد کنید.'})
+    rows, desc = execute(f"EXEC OrderNecessaryBloodProductsInCity @city_name_of_requester = N'{city_name}', @blood_type = N'{blood_type}'")
+    print('------------')    
+    print(rows)
+    print('------------')
+    return render(request, 'backend/blood_product_info.html', {'result': rows, 'titles': desc})
 
 
 def get_blood_transporter_info(request):
